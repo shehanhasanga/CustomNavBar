05c928604984e911d6a8c9bc6561e6c5d1b5484b
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-17 12:59:16 +0530

    Initial Commit

---
+++ CustomNavView/CustomNavViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavViewApp.swift
+//  CustomNavView
+//
+//  Created by shehan karunarathna on 2022-02-17.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
